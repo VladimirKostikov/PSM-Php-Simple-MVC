942ac138f6cf502a87e1d26244370b4698de4414
Vladimir Kostikov <86430748+VladimirKostikov@users.noreply.github.com>
2024-01-20 19:19:53 +0300

    DB Class added

--- app/Classes/DB.php
+++ app/Classes/DB.php
@@ -2,10 +2,39 @@
 
 namespace App\Classes;
 
+use \PDO;
 class DB {
-    private $db;
+    private $hostname;
+    private $login;
+    private $psw;
+    private $database;
 
-    public function __construct() {
-        
+    private $db;
+    
+
+    public function __construct($hostname, $login, $psw, $database) {
+        $this->hostname = $hostname;
+        $this->login = $login;
+        $this->psw = $psw;
+        $this->database = $database;
+    }
+
+    public function connect() {
+        $dsn = 'mysql:dbname='.$this->database.';host='.$this->hostname;
+        try 
+        {
+            $this->db = new PDO($dsn, $this->login, $this->password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->db->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+        }
+        catch (PDOException $e) 
+        {
+            echo 'DB Error';
+            die();
+        }
+    }
+
+    public function close() {
+        $this->db = null;
     }
 }
