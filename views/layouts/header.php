2d266159ec83b5e24a2c83d2a4494e50d5b23eea
Vladimir Kostikov <86430748+VladimirKostikov@users.noreply.github.com>
2024-01-23 00:10:17 +0300

    README added

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -4,19 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Header</title>
+    <title>Simple MVC Panel</title>
     <link rel="stylesheet" href="/public/css/style.css">
-
     <script src="https://cdn.tailwindcss.com"></script>
-    <script>
-        tailwind.config = {
-        theme: {
-            extend: {
-                
-            }
-        }
-        }
-    </script>
     
 </head>
 <body class="bg-slate-100">
